8f4e59efd2615771d8fb399cb89e1500693d35b2
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-09-09 05:27:43 -0400

    V1 Completed
    - Fetched api data
    - Displayed necessary information from api
    - Created search movie component
    - Created Card Component
    - Fully responsive CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import SearchMovies from "./components/SearchMovies"
 import './App.css'
 
-function App() {
-  return(
-    <div>
-      <p>hi</p>
-    </div>
-  )
-
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">React Movie Search</h1>
+        <SearchMovies />
+      </div>
+    )
+  }
 }
 
 export default App
